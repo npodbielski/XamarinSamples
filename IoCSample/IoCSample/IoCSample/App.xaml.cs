ae0c289edb16fd78626e503241f05507ea79c8c1
npodbielski <n.podbielski@gmail.com>
2016-12-10 16:55:24 +0100

    IoC sample added

---
+++ IoCSample/IoCSample/IoCSample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using TinyIoC;
+
+namespace IoCSample
+{
+    public partial class App
+    {
+        public static TinyIoCContainer IoC = new TinyIoCContainer();
+        public App ()
+        {
+            IoC.AutoRegister();
+            InitializeComponent ();
+            //IoC.Register<MainPage>();
+            //IoC.Register<MainPageViewModel>();
+            MainPage = IoC.Resolve<MainPage>();
+            //AppDomain
+            MainPage.BindingContext = IoC.Resolve<MainPageViewModel>();
+        }
+    }
+}
---
+++ IoCSample/IoCSample/IoCSample/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoCSample
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            MainText = "Main Page Text";
+        }
+        public string MainText { get; set; }
+    }
+}
